69bc4eac5def0acb7c276806769c63d270618557
VladislavT <vt@burno.tech>
2024-09-18 12:10:21 +0300

    Fixed indents for config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,37 +2,37 @@ import {defineNuxtConfig} from "nuxt/config"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: [
-    '@crazydos/nuxt-msw',
-    '@pinia/nuxt'
-  ],
-  msw : {
-    enable: true,
-    folderPath: "~/msw",
-  },
-  app: {
-    pageTransition: {
-      name: 'page',
-      mode: 'out-in',
-    }
-  },
-  css: [
-    '~/shared/styles/reset.scss',
-    '~/shared/styles/page-transition.scss',
-    '~/shared/styles/global.scss',
-  ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "@/shared/styles/vars/_index.scss";'
-        }
-      }
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: [
+        '@crazydos/nuxt-msw',
+        '@pinia/nuxt'
+    ],
+    msw : {
+        enable: true,
+        folderPath: "~/msw",
     },
-  },
-  devServer: {
-    port: 8000
-  },
+    app: {
+        pageTransition: {
+            name: 'page',
+            mode: 'out-in',
+        }
+    },
+    css: [
+        '~/shared/styles/reset.scss',
+        '~/shared/styles/page-transition.scss',
+        '~/shared/styles/global.scss',
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/shared/styles/vars/_index.scss";'
+                }
+            }
+        },
+    },
+    devServer: {
+        port: 8000
+    },
 })
